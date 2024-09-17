eeb85691d075dfb460e9e46f0ee79b91ef69a618
giolga <giolga.kumi@gmail.com>
2024-09-17 17:16:15 +0400

    first commit

---
+++ MVCLibrary/Models/SeedData.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCLibrary.Data;
+
+namespace MVCLibrary.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new LibraryContext(
+                serviceProvider.GetRequiredService<DbContextOptions<LibraryContext>>()))
+            {
+                if(context.Book.Any())
+                {
+                    return;
+                }
+
+                context.Book.AddRange(
+                    new Book { Title = "Tiny C# Projects", CallNumber = "AXD 20254"},
+                    new Book { Title = "Tiny android Projects", CallNumber = "AKA 2282"}
+                    );
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
